5c1ad7d2b79beeaf8f9db6f68361842934e75699
Ifrat Binte <ifratbinte@gmail.com>
2022-05-10 01:02:36 +0600

    Add skills

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
 /*
 ================================================================
-* Template:  	 Simone - Personal Portfolio Template
-* Written by: 	 Harnish Design - (http://www.harnishdesign.net)
+* Template:  	 Portfolio - Personal Portfolio Template
+* Written by: 	 Ifrat Binte
 * Description:   Main Custom Script File
 ================================================================
 */
@@ -67,68 +67,6 @@ $(".navbar-nav a").on('click', function() {
     $(".navbar-collapse, .navbar-toggler").removeClass("show");
 });
 
-// Overlay Menu & Side Open Menu
-$('.navbar-side-open .collapse, .navbar-overlay .collapse').on('show.bs.collapse hide.bs.collapse', function(e) {
-    e.preventDefault();
-}),
-$('.navbar-side-open [data-toggle="collapse"], .navbar-overlay [data-toggle="collapse"]').on('click', function(e) {
-   e.preventDefault();
-   $($(this).data('target')).toggleClass('show');
-})
-
-/*---------------------------------
-   Carousel (Owl Carousel)
------------------------------------ */
-$(".owl-carousel").each(function (index) {
-    var a = $(this);
-	$(this).owlCarousel({
-		autoplay: a.data('autoplay'),
-		center: a.data('center'),
-		autoplayTimeout: a.data('autoplaytimeout'),
-		autoplayHoverPause: a.data('autoplayhoverpause'),
-		loop: a.data('loop'),
-		speed: a.data('speed'),
-		nav: a.data('nav'),
-		dots: a.data('dots'),
-		autoHeight: a.data('autoheight'),
-		autoWidth: a.data('autowidth'),
-		margin: a.data('margin'),
-		stagePadding: a.data('stagepadding'),
-		slideBy: a.data('slideby'),
-		lazyLoad: a.data('lazyload'),
-		navText:['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-		animateOut: a.data('animateout'),
-		animateIn: a.data('animatein'),
-		video: a.data('video'),
-		items: a.data('items'),
-		responsive:{
-        0:{items: a.data('items-xs'),},
-        576:{items: a.data('items-sm'),},
-		768:{items: a.data('items-md'),},
-        992:{items: a.data('items-lg'),}
-        }
-    });
-});
-
-/*------------------------------------
-    Magnific Popup
--------------------------------------- */
-// Image on Modal
-$('.popup-img-gallery').each(function() {
-$(this).magnificPopup({
-    delegate: '.popup-img:visible',
-	type: "image",
-	tLoading: '<div class="preloader"><div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>',
-    closeOnContentClick: !0,
-    mainClass: "mfp-fade",
-    gallery: {
-        enabled: true,
-        navigateByImgClick: true,
-        preload: [0, 1]
-    },
-});
-});
-
 // Ajax On Modal 
 $('.popup-ajax-gallery').each(function() {
 $(this).magnificPopup({
@@ -178,15 +116,6 @@ $(this).magnificPopup({
 });
 });
 
-// YouTube/Viemo Video & Gmaps
-$('.popup-youtube, .popup-vimeo, .popup-gmaps').each(function() {
-$(this).magnificPopup({
-        type: 'iframe',
-		mainClass: 'mfp-fade',
-});
-});
-
-
 /*------------------------------------
     Isotope Portfolio Filter
 -------------------------------------- */
@@ -216,25 +145,6 @@ $(this).parallaxie({
 });
 });
 
-/*------------------------------------
-    Counter
--------------------------------------- */
-$(".counter").each(function () {
-    $(this).appear(function () {
-        $(this).countTo({
-			speed: 1800,
-		});
-    });
-});
-
-/*------------------------------------
-    Text Rotator
--------------------------------------- */
-$(".text-rotator").each(function () {
-    $(this).Morphext({
-		speed: 3000, // Overrides default 2000
-	});
-});
 
 /*------------------------------------
     Typed
@@ -248,31 +158,6 @@ var typed = new Typed('.typed', {
     backSpeed: 50,
 	backDelay: 1500,
 });
-});
-
-/*------------------------------------
-    WOW animation
--------------------------------------- */
-
-$(".wow").each(function() {
- if ($(window).width() > 767) {
-   var wow = new WOW({
-     boxClass: 'wow',
-     animateClass: 'animated',
-     offset: 0,
-     mobile: false,
-     live: true
-   });
-  new WOW().init();
- }
-});
-
-/*------------------------------------
-    YTPlayer YouTube Background
--------------------------------------- */
-
-$(".player").each(function () {
-    $(this).mb_YTPlayer();
 });
 
 /*------------------------
@@ -295,90 +180,6 @@ $(function () {
 $('#back-to-top').on("click", function() {
 	$('html, body').animate({scrollTop:0}, 'slow');
 	return false;
-});
-
-/*------------------------
-   Contact Form
--------------------------- */
-var form = $('#contact-form'); // contact form
-var submit = $('#submit-btn'); // submit button
-
-// form submit event
-form.on('submit', function (e) {
-	e.preventDefault(); // prevent default form submit
-
-	if (typeof $('#google-recaptcha-v3').val() != "undefined") {
-		grecaptcha.ready(function () {
-			var site_key = $('#google-recaptcha-v3').attr('src').split("render=")[1];
-			grecaptcha.execute(site_key, {action: 'contact'}).then(function (token) {
-				var gdata = form.serialize() + '&g-recaptcha-response=' + token;
-				$.ajax({
-					url: 'php/mail.php',  // form action url
-					type: 'POST', 		  // form submit method get/post
-					dataType: 'json', 	  // request type html/json/xml
-					data: gdata, 		  // serialize form data
-					beforeSend: function () {
-						submit.attr("disabled", "disabled");
-						var loadingText = '<span role="status" aria-hidden="true" class="spinner-border spinner-border-sm align-self-center mr-2"></span>Sending.....'; // change submit button text
-						if (submit.html() !== loadingText) {
-							submit.data('original-text', submit.html());
-							submit.html(loadingText);
-						}
-					},
-					success: function (data) {
-						submit.before(data.Message).fadeIn("slow"); // fade in response data 
-						submit.html(submit.data('original-text'));// reset submit button text
-						submit.removeAttr("disabled", "disabled");
-						if (data.response == 'success') {
-							form.trigger('reset'); // reset form
-						}
-						setTimeout(function () {
-							$('.alert-dismissible').fadeOut('slow', function(){
-								$(this).remove();
-							});
-						}, 3000);
-					},
-					error: function (e) {
-						console.log(e)
-					}
-				});
-			});
-		});
-	} else {
-		$.ajax({
-			url: 'php/mail.php', // form action url
-			type: 'POST', // form submit method get/post
-			dataType: 'json', // request type html/json/xml
-			data: form.serialize(), // serialize form data
-			beforeSend: function () {
-				submit.attr("disabled", "disabled");
-				var loadingText = '<span role="status" aria-hidden="true" class="spinner-border spinner-border-sm align-self-center mr-2"></span>Sending.....'; // change submit button text
-				if (submit.html() !== loadingText) {
-					submit.data('original-text', submit.html());
-					submit.html(loadingText);
-				}
-			},
-			success: function (data) {
-				submit.before(data.Message).fadeIn("slow"); // fade in response data 
-				submit.html(submit.data('original-text'));// reset submit button text
-				submit.removeAttr("disabled", "disabled");
-				if (data.response == 'success') {
-					form.trigger('reset'); // reset form
-				}
-				setTimeout(function () {
-					$('.alert-dismissible').fadeOut('slow', function(){
-						$(this).remove();
-					});
-				}, 3000);
-				if (typeof $('#recaptcha-v2').val() != "undefined") {
-					grecaptcha.reset(); // reset reCaptcha
-				}
-			},
-			error: function (e) {
-				console.log(e)
-			}
-		});
-	}
 });
 
 })(jQuery)
